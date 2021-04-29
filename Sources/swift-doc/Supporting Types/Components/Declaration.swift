e3775aadae3dedbb8ab3f97e9a03103423a0342b
Karl <5254025+karwa@users.noreply.github.com>
2021-04-29 20:24:51 +0200

    Add a basic filter to fix indentation of function names

--- Sources/swift-doc/Supporting Types/Components/Declaration.swift
+++ Sources/swift-doc/Supporting Types/Components/Declaration.swift
@@ -28,7 +28,32 @@ struct Declaration: Component {
     }
 
     var html: HypertextLiteral.HTML {
-        let code = symbol.declaration.map { $0.html }.joined()
+
+        var filteredDeclaration = [Token]()
+
+        // This is a very basic, hacky way to strip extra space tokens at the start of a line.
+        // It may not work for everything, but it's better than doing nothing.
+        var hasYieldedTextOnLine = false
+        var indentLevel = 0
+        for token in symbol.declaration {
+          if token.text == "(" { indentLevel += 1 }
+          if token.text == ")" { indentLevel = max(indentLevel - 1, 0) }
+
+          if token.text.allSatisfy({ $0 == " " }), !hasYieldedTextOnLine { continue }
+
+          guard token.text != "\n" else {
+            hasYieldedTextOnLine = false
+            filteredDeclaration.append(token)
+            continue
+          }
+          if !hasYieldedTextOnLine {
+            filteredDeclaration.append(contentsOf: repeatElement(Token("  ", kind: Text.self), count: indentLevel))
+            hasYieldedTextOnLine = true
+          }
+          filteredDeclaration.append(token)
+        }
+
+        let code = filteredDeclaration.map { $0.html }.joined()
 
         return #"""
         <div class="declaration">
